96d8a379f782ca28a2e039b0c3f1c14812560fe1
hoangbros03 <87892619+hoangbros03@users.noreply.github.com>
2022-05-12 21:25:44 +0700

    TEST
    TEST

--- SDL/Map.cpp
+++ SDL/Map.cpp
@@ -52,26 +52,12 @@ void Map::LoadMap(std::string path, int sizeX, int sizeY)
 			mapFile.ignore();
 
 		}
-		mapFile.ignore();
+		//mapFile.ignore();
 	}
 
 	mapFile.ignore();
 
-	for (int y = 0; y < sizeY; y++)
-	{
-		for (int x= 0; x< sizeX; x++)
-		{
-			mapFile.get(c);
-			if (c == '2')
-			{
-				auto& tcol(manager.addEntity());
-				tcol.addComponent<CollisionComponent>("terrain", x * scaledSize, y * scaledSize, scaledSize);
-				tcol.addGroup(Game::groupColliders);
-			}
-			mapFile.ignore();
-		}
-		mapFile.ignore();
-	}
+
 
 	mapFile.close();
 }
